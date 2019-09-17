d63958be7a31416e390a8bd81a7f166ba4d2f621
EyeShield-0 <angelnaguit@icloud.com>
2019-09-17 11:27:50 -0400

    3 files modified, 3 added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({ providedIn:'root'})
 export class CartService {
   items = [];
+  
+  constructor( private http: HttpClient ) { }
 
   addToCart(product){
     this.items.push(product);
@@ -17,11 +20,14 @@ export class CartService {
     return this.items;
   }
 
+  getShippingPrices() {
+    return this.http.get('/assets/shipping.json');
+  }
 
 
 
 
 
-  constructor() { }
+
 
 }
